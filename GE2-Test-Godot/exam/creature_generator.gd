f73e6185e2b2da2c6c6707d1087dbb41ab7ad94b
MossDog <lukejohnmosshughes@gmail.com>
2024-04-23 11:39:48 +0100

    improved sin calculation

--- GE2-Test-Godot/exam/creature_generator.gd
+++ GE2-Test-Godot/exam/creature_generator.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
-@export var length = 0
+@export var length = 200
 @export var start_angle = 0
-@export var frequency = 0
-@export var base_size = 0
+@export var frequency = 2
+@export var base_size = 15
 @export var mult = 0
 @export var headScn:PackedScene
 @export var bodyScn:PackedScene
@@ -11,13 +11,13 @@ var body_instances = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var head = headScn.instantiate()
-	add_child(head)
+	var head_instance = headScn.instantiate()
+	add_child(head_instance)
 	
 	for i in range(length):
 		var body_instance = bodyScn.instantiate()
-		head.add_child(body_instance)
-		body_instance.global_transform.origin.x = head.global_transform.origin.x - (5000 * i)
+		head_instance.add_child(body_instance)
+
 		body_instances.append(body_instance)
 		pass
 
@@ -26,7 +26,9 @@ func _ready():
 func _process(delta):
 	for i in range(body_instances.size()):
 		var body_instance = body_instances[i]
-		var size = base_size * sin(frequency + i)
-		body_instance.scale = Vector3(base_size, base_size, base_size)
+		var size = base_size * sin(i)
+		body_instance.size = Vector3(size, size, size)
+		body_instance.global_transform.origin.x = body_instance.get_parent().global_transform.origin.x - body_instance.get_size().x
+		
 		
 	pass
